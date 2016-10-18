79b4de3ac3ce31fd1c60688eda006bba3a3adaf9
Jamal McDowell <jamal.l.mcdowell@gmail.com>
2016-10-18 15:20:14 -0400

    Added text for equipment

--- src/main/webapp/js/fitnessapp.js
+++ src/main/webapp/js/fitnessapp.js
@@ -1,9 +1,10 @@
-$("#reset-table").click(function () {
-  location.reload();});
+$("#reset-table").click(function() {
+    location.reload();
+});
 
 $("#equipment-submit").click(function() {
 
-//$("#tableData").empty();
+    //$("#tableData").empty();
 
     //equipment selectd
     var equipment = $(".equipment:checked");
@@ -11,6 +12,7 @@ $("#equipment-submit").click(function() {
     var exerciseData = [];
     //place to store all my promises
     var promises = [];
+    var equipmentText=[];
 
     //loop over the equipment and make API calls
     equipment.each(function() {
@@ -25,6 +27,9 @@ $("#equipment-submit").click(function() {
             //let it know the promise is resolved
             def.resolve();
         });
+
+
+
         //push promise into promises array
         promises.push(def);
     });
@@ -33,14 +38,89 @@ $("#equipment-submit").click(function() {
     $.when.apply($, promises).then(function() {
         $.each(exerciseData, function(key, val) {
             if (typeof val.name != 'undefined') {
-                console.log(val.name + " - " + val.description);
+                switch (val.category) {
+                    case 10:
+                        val.category = "Abs";
+                        break;
 
-                $('#tableData').append("<tr><td>" + val.name + "</td><td>" + val.description + "</td><td>" + val.category+ "</td><td>" + val.equipment + "</td></tr>");
+                    case 8:
+                        val.category = "Arms";
+                        break;
+
+                    case 12:
+                        val.category = "Back";
+                        break;
+
+                    case 14:
+                        val.category = "Calves";
+                        break;
+
+                    case 11:
+                        val.category = "Chest";
+                        break;
+
+                    case 9:
+                        val.category = "Legs";
+                        break;
+
+                    case 13:
+                        val.category = "Shoulders";
+                        break;
+
+                    default:
+                }
+
+                $.each(val.equipment,function(key,val) {
+
+                  switch (val) {                      case 1:
+                          equipmentText.push("Barbell") ;
+                          break;
+
+                      case 8:
+                          equipmentText.push( "Bench");
+                          break;
+
+                      case 3:
+                          equipmentText.push("Dumbbell");
+                          break;
+
+                      case 4:
+                          equipmentText.push( "Gym Mat");
+                          break;
+
+                      case 9:
+                          equipmentText.push( "Incline Bench");
+                          break;
+
+                      case 10:
+                          equipmentText.push("Kettlebell");
+                          break;
+
+                      case 7:
+                          equipmentText.push( "No Equipment Needed");
+                          break;
+
+                      case 6:
+                          equipmentText.push("Pull-Up Bar");
+                          break;
+                      case 2:
+                          equipmentText.push( "SZ-Bar");
+                          break;
+                      default:
+
+                  }
+
+                });
+
+
+                $('#tableData').append("<tr><td>" + val.name + "</td><td>" + val.description + "</td><td>" + val.category + "</td><td>" + equipmentText + "</td></tr>");
+
+                equipmentText=[];
             }
 
         });
     }).then(function() {
-      $('#exerciseTable').DataTable();
+        $('#exerciseTable').DataTable();
     });
 
 
